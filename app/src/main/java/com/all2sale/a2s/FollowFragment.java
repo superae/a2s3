64a89ea413ec5a530c439106a3447a3371ce9103
superae <osx@Imac-goodx.local>
2016-06-17 16:43:11 +0700

    v1

---
+++ app/src/main/java/com/all2sale/a2s/FollowFragment.java
@@ -0,0 +1,29 @@
+package com.all2sale.a2s;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FollowFragment extends Fragment {
+
+
+    public FollowFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_follow, container, false);
+    }
+
+}
