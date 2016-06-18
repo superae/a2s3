b4fba6687c6f2a4ab39dfbf59c29886b3632fbc6
superae <osx@Imac-goodx.local>
2016-06-18 16:54:33 +0700

    1.4

---
+++ app/src/main/java/com/all2sale/a2s/FillterActivity.java
@@ -0,0 +1,35 @@
+package com.all2sale.a2s;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class FillterActivity extends AppCompatActivity {
+
+//    private ImageButton button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filter_product);
+
+//        button3 = (ImageButton) findViewById(R.id.close_filter);
+//
+//        button3.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View v){
+//
+////                Intent i = new Intent(FillterActivity.this, ProductActivity.class);
+////
+////                startActivity(i);
+//
+//            }
+//
+//        });
+
+
+    }
+}
--- app/src/main/java/com/all2sale/a2s/ProductActivity.java
+++ app/src/main/java/com/all2sale/a2s/ProductActivity.java
@@ -1,15 +1,23 @@
 package com.all2sale.a2s;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ProductActivity extends AppCompatActivity {
+
+    private ImageButton button1;
+    private ImageButton button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,54 @@ public class ProductActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        button1 = (ImageButton) findViewById(R.id.sort_popup);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(ProductActivity.this, button1);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater()
+                        .inflate(R.menu.sort_list, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(
+                                ProductActivity.this,
+                                item.getTitle(),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                        return true;
+                    }
+                });
+
+                popup.show(); //showing popup menu
+            }
+        }); //closing the setOnClickListener method
+
+
+        button2 = (ImageButton) findViewById(R.id.imageButton4);
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+
+                Intent i = new Intent(ProductActivity.this, FillterActivity.class);
+
+                startActivity(i);
+
+                overridePendingTransition(R.anim.slide_left,R.anim.slide_left);
+
+
+            }
+
+        });
+
+
+
     }
 
     @Override
